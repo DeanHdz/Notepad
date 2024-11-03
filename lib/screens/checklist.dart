7c7b2bd5a8ddca08d8a44702614015428b6e128e
DeanHdz <dean2103@gmail.com>
2024-11-03 07:29:17 -0600

    Esqueleto de vistas nota y listado

--- lib/screens/checklist.dart
+++ lib/screens/checklist.dart
@@ -10,6 +10,31 @@ class Checklist extends StatefulWidget {
 class _ChecklistState extends State<Checklist> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3B3B3B),
+        iconTheme: const IconThemeData(color: Colors.white),
+        centerTitle: true,
+        title: const Text('Listado de tareas',
+            style: TextStyle(color: Colors.white, fontSize: 32)),
+      ),
+      backgroundColor: const Color(0xFF252525),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Titulo',
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/note.dart
+++ lib/screens/note.dart
@@ -10,6 +10,38 @@ class Note extends StatefulWidget {
 class _NoteState extends State<Note> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3B3B3B),
+        iconTheme: const IconThemeData(color: Colors.white),
+        centerTitle: true,
+        title: const Text('Nota',
+            style: TextStyle(color: Colors.white, fontSize: 32)),
+      ),
+      backgroundColor: const Color(0xFF252525),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Titulo',
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            const Text(
+              'Escribe algo...',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
